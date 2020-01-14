34794fbd8decfbcf0f6b8498f4ea72545df4e7d8
Karol Golec <karolgolec10@gmail.com>
2020-01-14 11:35:27 +0100

    Default config add

--- config/fair-queue.php
+++ config/fair-queue.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
-    //
+
+    /*
+     |--------------------------------------------------------------------------
+     | Default models with identifier
+     |
+     | Model must contains ID column. Model selected from below `models` array.
+     |--------------------------------------------------------------------------
+     */
+    'default_model' => 'user',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Models with identifier
+     |
+     | Models must contains ID column
+     |--------------------------------------------------------------------------
+     */
+    'models' => [
+        'user' => 'App\User',
+    ]
 ];
